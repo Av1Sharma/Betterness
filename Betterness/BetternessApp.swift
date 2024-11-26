2a9cf12cac893a334df5d1d3a6a13c7a0e09e5dd
Avi Sharma <64670742+Av1Sharma@users.noreply.github.com>
2024-11-25 20:10:42 -0500

    Initial Commit

---
+++ Betterness/BetternessApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetternessApp.swift
+//  Betterness
+//
+//  Created by Avi Sharma on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BetternessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetternessTests/BetternessTests.swift
@@ -0,0 +1,17 @@
+//
+//  BetternessTests.swift
+//  BetternessTests
+//
+//  Created by Avi Sharma on 11/25/24.
+//
+
+import Testing
+@testable import Betterness
+
+struct BetternessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
